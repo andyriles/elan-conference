72e3ff59ed0572f1327562852c64684b7ddb0f76
Andrew Efurhievwe <andyriles22@gmail.com>
2020-11-04 20:08:41 +0100

    updated .env file

--- src/fire.js
+++ src/fire.js
@@ -4,10 +4,10 @@ var firebaseConfig = {
     apiKey: `${process.env.REACT_APP_API_KEY}`,
     authDomain: "login-66171.firebaseapp.com",
     databaseURL: "https://login-66171.firebaseio.com",
-    projectId: "login-66171",
+    projectId: `${process.env.REACT_APP_PROJECT_ID}`,
     storageBucket: "login-66171.appspot.com",
-    messagingSenderId: "1084368324579",
-    appId: "1:1084368324579:web:4185a6a96044ed775bc2d6"
+    messagingSenderId: `${process.env.REACT_APP_MESSANGER_ID}`,
+    appId: `${process.env.REACT_APP_ID}`
   };
 
  const fire= firebase.initializeApp(firebaseConfig);
